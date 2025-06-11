be97d4ada3b72ffe4c6ce7cba2b664bc6abf5fe3
bharatparakh <bharatparakh15@gmail.com>
2025-06-11 19:23:36 +0530

    Added api-gateway module

--- user-service/src/main/java/com/aifinance/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/aifinance/user_service/UserServiceApplication.java
@@ -3,11 +3,21 @@ package com.aifinance.user_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
+@RequestMapping("/api/users")
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayhello(){
+        return "hiiii";
     }
 }
